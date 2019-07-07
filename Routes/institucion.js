bb8a4c6e0d7190d604576980339d9bbe9c37db17
Oriana Rodríguez <oriana.b.rodriguez.m@gmail.com>
2019-07-06 22:22:38 -0400

    Add files via upload

---
+++ Routes/institucion.js
@@ -0,0 +1,239 @@
+var express = require('express')
+var app = express()
+ 
+app.get('/', function(req, res, next) {
+    req.getConnection(function(error, conn) {
+        conn.query('SELECT * FROM users ORDER BY id DESC',function(err, rows, fields) {
+            //if(err) throw err
+            if (err) {
+                req.flash('error', err)
+                res.render('user/list', {
+                    title: 'User List', 
+                    data: ''
+                })
+            } else {
+                // render to views/user/list.ejs template file
+                res.render('user/list', {
+                    title: 'User List', 
+                    data: rows
+                })
+            }
+        })
+    })
+})
+ 
+// SHOW ADD USER FORM
+app.get('/add', function(req, res, next){    
+    // render to views/user/add.ejs
+    res.render('user/add', {
+        title: 'Añadir nueva institución',
+        nombre: '',
+        descripcion: '',
+        telefono: '',        
+        correo: '',
+        direccion:''
+    })
+})
+ 
+// ADD NEW USER POST ACTION
+app.post('/add', function(req, res, next){    
+    req.assert('Nombre', 'Es requerido un nombre').notEmpty()           //Validate name
+    req.assert('Descripcion', 'Es requerida una descripción').notEmpty()             //Validate age
+    req.assert('Telefono', 'Es requerido un número de teléfono valido').isEmail()  //Validate email
+    req.assert('Correo', 'Es requerida una dirección de correo electrónico valida').notEmpty()
+    req.assert('Direccion', 'Es requerida una direccion').notEmpty()
+ 
+    var errors = req.validationErrors()
+    
+    if( !errors ) {   //No errors were found.  Passed Validation!
+        
+        /********************************************
+         * Express-validator module
+         
+        req.body.comment = 'a <span>comment</span>';
+        req.body.username = '   a user    ';
+ 
+        req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
+        req.sanitize('username').trim(); // returns 'a user'
+        ********************************************/
+        var institucion = {
+            nombre: req.sanitize('Nombre').escape().trim(),
+            descripcion: req.sanitize('Descripción').escape().trim(),
+            telefono: req.sanitize('Teléfono').escape().trim(),
+            correo: req.sanitize('Correo').escape().trim(),
+            direccion: req.sanitize('Dirección').escape().trim()
+        }
+        
+        req.getConnection(function(error, conn) {
+            conn.query('INSERT INTO users SET ?', user, function(err, result) {
+                //if(err) throw err
+                if (err) {
+                    req.flash('error', err)
+                    
+                    // render to views/user/add.ejs
+                    res.render('user/add', {
+                        title: 'Añadir nueva institución',
+                        nombre: institucion.nombre,
+                        descripcion: institucion.descripcion,
+                        telefono: institucion.telefono,
+                        correo: institucion.correo,
+                        direccion: institucion.direccion
+                    })
+                } else {                
+                    req.flash('Éxito', 'Se ha añadido exitosamente una institución')
+                    
+                    // render to views/user/add.ejs
+                    res.render('user/add', {
+                        title: 'Añadir nueva institución',
+                        nombre: '',
+                        descripcion: '',
+                        telefono: '',                    
+                        correo: '',
+                        direccion: ''
+                    })
+                }
+            })
+        })
+    }
+    else {   //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + '<br>'
+        })                
+        req.flash('error', error_msg)        
+        
+        /**
+         * Using req.body.name 
+         * because req.param('name') is deprecated
+         */ 
+        res.render('user/add', { 
+            title: 'Add New User',
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        })
+    }
+})
+ 
+// SHOW EDIT USER FORM
+app.get('/edit/(:id)', function(req, res, next){
+    req.getConnection(function(error, conn) {
+        conn.query('SELECT * FROM users WHERE id = ' + req.params.id, function(err, rows, fields) {
+            if(err) throw err
+            
+            // if user not found
+            if (rows.length <= 0) {
+                req.flash('error', 'User not found with id = ' + req.params.id)
+                res.redirect('/users')
+            }
+            else { // if user found
+                // render to views/user/edit.ejs template file
+                res.render('user/edit', {
+                    title: 'Edit User', 
+                    //data: rows[0],
+                    id: rows[0].id,
+                    name: rows[0].name,
+                    age: rows[0].age,
+                    email: rows[0].email                    
+                })
+            }            
+        })
+    })
+})
+ 
+// EDIT USER POST ACTION
+app.put('/edit/(:id)', function(req, res, next) {
+    req.assert('name', 'Name is required').notEmpty()           //Validate name
+    req.assert('age', 'Age is required').notEmpty()             //Validate age
+    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+ 
+    var errors = req.validationErrors()
+    
+    if( !errors ) {   //No errors were found.  Passed Validation!
+        
+        /********************************************
+         * Express-validator module
+         
+        req.body.comment = 'a <span>comment</span>';
+        req.body.username = '   a user    ';
+ 
+        req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
+        req.sanitize('username').trim(); // returns 'a user'
+        ********************************************/
+        var user = {
+            name: req.sanitize('name').escape().trim(),
+            age: req.sanitize('age').escape().trim(),
+            email: req.sanitize('email').escape().trim()
+        }
+        
+        req.getConnection(function(error, conn) {
+            conn.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function(err, result) {
+                //if(err) throw err
+                if (err) {
+                    req.flash('error', err)
+                    
+                    // render to views/user/add.ejs
+                    res.render('user/edit', {
+                        title: 'Edit User',
+                        id: req.params.id,
+                        name: req.body.name,
+                        age: req.body.age,
+                        email: req.body.email
+                    })
+                } else {
+                    req.flash('success', 'Data updated successfully!')
+                    
+                    // render to views/user/add.ejs
+                    res.render('user/edit', {
+                        title: 'Edit User',
+                        id: req.params.id,
+                        name: req.body.name,
+                        age: req.body.age,
+                        email: req.body.email
+                    })
+                }
+            })
+        })
+    }
+    else {   //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + '<br>'
+        })
+        req.flash('error', error_msg)
+        
+        /**
+         * Using req.body.name 
+         * because req.param('name') is deprecated
+         */ 
+        res.render('user/edit', { 
+            title: 'Edit User',            
+            id: req.params.id, 
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        })
+    }
+})
+ 
+// DELETE USER
+app.delete('/delete/(:id)', function(req, res, next) {
+    var user = { id: req.params.id }
+    
+    req.getConnection(function(error, conn) {
+        conn.query('DELETE FROM users WHERE id = ' + req.params.id, user, function(err, result) {
+            //if(err) throw err
+            if (err) {
+                req.flash('error', err)
+                // redirect to users list page
+                res.redirect('/users')
+            } else {
+                req.flash('success', 'User deleted successfully! id = ' + req.params.id)
+                // redirect to users list page
+                res.redirect('/users')
+            }
+        })
+    })
+})
+ 
+module.exports = app
